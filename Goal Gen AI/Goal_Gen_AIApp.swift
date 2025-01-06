942d4bf368d7a13e8a7469cd537b73ceffc8d74d
eb-3 <eabu35@gmail.comeabu35@gmail.comeabu35@gmail.com>
2025-01-06 12:16:54 -0600

    Initial Commit

---
+++ Goal Gen AI/Goal_Gen_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Goal_Gen_AIApp.swift
+//  Goal Gen AI
+//
+//  Created by Earl Burl III on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Goal_Gen_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Goal Gen AITests/Goal_Gen_AITests.swift
@@ -0,0 +1,17 @@
+//
+//  Goal_Gen_AITests.swift
+//  Goal Gen AITests
+//
+//  Created by Earl Burl III on 1/6/25.
+//
+
+import Testing
+@testable import Goal_Gen_AI
+
+struct Goal_Gen_AITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
